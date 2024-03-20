23f2123cf271e7127c478a0881077ff2349b97bb
Deepak Jayanna <88205314+djayanna@users.noreply.github.com>
2024-03-20 18:50:31 -0400

    config update

--- config.py
+++ config.py
@@ -23,3 +23,12 @@ def get_weights_file_path(config, epoch: str):
     model_basename = config['model_basename']
     model_filename = f"{model_basename}{epoch}.pth"
     return str(Path('.') / model_folder / model_filename)
+
+def latest_weights_file_path(config):
+    model_folder = f"{config['datasource']}_{config['model_folder']}"
+    model_filename = f"{config['model_basename']}*"
+    weights_files = list(Path(model_folder).glob(model_filename))
+    if len(weights_files) == 0:
+        return None
+    weights_files.sort()
+    return str(weights_files[-1])
